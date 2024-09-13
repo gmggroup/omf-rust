cbbde3133337d3c9a9f0679f6ccd208f005b7a9e
Ant Brown <ant@catalyst.net.nz>
2024-09-13 14:02:20 +1200

    Add PyIOError to use statements

--- omf-python/src/omf1/converter.rs
+++ omf-python/src/omf1/converter.rs
@@ -1,4 +1,5 @@
 use omf::omf1::detect_open;
+use pyo3::exceptions::PyIOError;
 use pyo3::prelude::*;
 use std::path::Path;
 
@@ -7,6 +8,6 @@ pub fn detect_omf1(path: String) -> PyResult<bool> {
     let path = Path::new(&path);
     match detect_open(path) {
         Ok(result) => Ok(result),
-        Err(e) => Err(PyErr::new::<pyo3::exceptions::PyIOError, _>(e.to_string())),
+        Err(e) => Err(PyErr::new::<PyIOError, _>(e.to_string())),
     }
 }
