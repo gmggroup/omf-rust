b6b9fa6c8f8e2106c17b2dbea00ca20b837e41d1
Ant Brown <ant@catalyst.net.nz>
2024-09-16 12:59:24 +1200

    Split Reader up so it can raise a file io error separately

--- omf-python/tests/test_reader.py
+++ omf-python/tests/test_reader.py
@@ -1,4 +1,5 @@
 import omf_python
+import pytest
 from os import path
 from unittest import TestCase
 
@@ -6,8 +7,8 @@ from unittest import TestCase
 class TestReader(TestCase):
     def setUp(self) -> None:
         self.examples_dir = path.join(path.dirname(__file__), "../../examples")
-    
-    def test_should_return_expected_project_name(self) -> None:  
+
+    def test_should_return_expected_project_name(self) -> None:
         # Given
         omf_file = path.join(self.examples_dir, "pyramid/pyramid.omf")
 
@@ -16,7 +17,15 @@ class TestReader(TestCase):
 
         # Then
         self.assertEqual(reader.project.name, "Pyramid")
-        
+
     def test_should_raise_expected_file_not_found_exception(self) -> None:
-        # TODO: currently raises pyo3_runtime.PanicException, should raise a sensible exception here
-        pass
+        # Given
+        incorrect_location = path.join(self.examples_dir, "testfilenotfound.omf")
+
+        # When
+        with pytest.raises(OSError) as exception:
+            reader = omf_python.Reader(incorrect_location)
+
+        # Then
+        self.assertEqual(str(exception.value), "No such file or directory (os error 2)")
+
