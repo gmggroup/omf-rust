f771b15e2e003be1aa81e3249e564411a6c44c55
Gary Lockyer <gary@catalyst.net.nz>
2024-09-18 12:54:39 +1200

    CENPLAT-23925 Arrays add item_count
    Add the item_count getter to PyArrayVertex, PyArrayIndex and
    PyArrayTriangle.

--- omf-python/src/array.rs
+++ omf-python/src/array.rs
@@ -5,13 +5,34 @@ use pyo3::prelude::*;
 pub struct PyArrayVertex {
     pub inner: Array<array_type::Vertex>,
 }
+#[pymethods]
+impl PyArrayVertex {
+    #[getter]
+    fn item_count(&self) -> u64 {
+        self.inner.item_count()
+    }
+}
 
 #[pyclass(name = "ArrayIndex")]
 pub struct PyArrayIndex {
     pub inner: Array<array_type::Index>,
 }
+#[pymethods]
+impl PyArrayIndex {
+    #[getter]
+    fn item_count(&self) -> u64 {
+        self.inner.item_count()
+    }
+}
 
 #[pyclass(name = "ArrayTriangle")]
 pub struct PyArrayTriangle {
     pub inner: Array<array_type::Triangle>,
+}
+#[pymethods]
+impl PyArrayTriangle {
+    #[getter]
+    fn item_count(&self) -> u64 {
+        self.inner.item_count()
+    }
 }
