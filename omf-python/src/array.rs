8149b2b0101d56daf92f76924cb9a09c552682b4
Ant Brown <ant@catalyst.net.nz>
2024-09-27 10:02:20 +1200

    CENPLAT-24001: Add item_count to BoundaryArray using macro

--- omf-python/src/array.rs
+++ omf-python/src/array.rs
@@ -67,6 +67,11 @@ pub struct PyTextArray(pub Array<array_type::Text>);
 /// Nullable booleans.
 pub struct PyBooleanArray(pub Array<array_type::Boolean>);
 
+#[gen_stub_pyclass]
+#[pyclass(name = "BoundaryArray")]
+/// Discrete color-map boundaries.
+pub struct PyBoundaryArray(pub Array<array_type::Boundary>);
+
 macro_rules! array_type_impl {
     ($($arrayType:ty)*) => ($(
         #[gen_stub_pymethods]
@@ -94,16 +99,5 @@ array_type_impl! {
     PyVectorArray
     PyTextArray
     PyBooleanArray
-}
-
-#[gen_stub_pyclass]
-#[pyclass(name = "BoundaryArray")]
-/// Discrete color-map boundaries.
-pub struct PyBoundaryArray(pub Array<array_type::Boundary>);
-#[pymethods]
-impl PyBoundaryArray {
-    #[getter]
-    fn item_count(&self) -> u64 {
-        self.0.item_count()
-    }
+    PyBoundaryArray
 }
